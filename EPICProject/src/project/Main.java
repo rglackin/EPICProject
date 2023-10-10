cd86479f7f272084f311e6097bd13a21ae487efa
Ruairí Glackin <ruairiglackin@gmail.com>
2023-10-10 15:53:56 +0100

    commit

--- EPICProject/src/project/Main.java
+++ EPICProject/src/project/Main.java
@@ -2,6 +2,8 @@ package project;
 
 public class Main {
     public static void main(String[] args) {
-        
+        //ask user to input username
+        //ask to input password
+        //call checkdetails method
     }
 }
--- EPICProject/src/project/User.java
+++ EPICProject/src/project/User.java
@@ -1,7 +1,14 @@
 package project;
 
 public class User {
-    public static void main(String[] args) {
-        
+    //username and password variables
+    private String username;
+    public String getUsername() {
+        return username;
     }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    //check details are same as in text file
+
 }
