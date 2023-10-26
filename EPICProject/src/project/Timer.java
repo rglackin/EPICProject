be19785fc39696102fa5375ab234e69075d90646
Tomas <deanetomas8@gmail.com>
2023-10-26 21:39:50 +0100

    preparing to move timer functions into questionAnswerBank with the other quiz formats, this branch will be repurposed

--- EPICProject/src/project/Timer.java
+++ EPICProject/src/project/Timer.java
@@ -2,20 +2,18 @@ package project;
 
 public class Timer {
 	
-	/* In main method:
-	*  Timer quizTimer = new Timer();
-	*  quizTimer.start();
-	*  double elapsedTime = quizTimer.end();
-	*/
-	
-	private long startTime;
-	
 	public void startTimer() { //method to begin timer
-		startTime = System.currentTimeMillis();
+		long startTime = System.currentTimeMillis();
 	}
+	
 	public double end() { //method to end timer and return elapsed time
         long endTime = System.currentTimeMillis();
         long elapsedTime = endTime - startTime;
         return elapsedTime / 1000.0;
 	}
+	/* In main method:
+	*  Timer quizTimer = new Timer();
+	*  quizTimer.start();
+	*  double elapsedTime = quizTimer.end();
+	*/
 }
