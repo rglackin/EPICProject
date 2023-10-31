1e547f868f422b3e7f6c5919862f22f6f107caa2
Tomas <deanetomas8@gmail.com>
2023-10-31 22:35:31 +0000

    formatting

--- EPICProject/src/project/passRegex.java
+++ EPICProject/src/project/passRegex.java
@@ -4,9 +4,11 @@ import java.util.regex.Pattern;
 
 public class passRegex {
 	
-	public static boolean validPassword(String password) { //boolean return function to validate password based on parameters, takes password as string as argument
-		String string = password;							//must be at least 8 characters, must contain uppercase, lowercase letters, number, special character
-															//returns true if password is valid, false otherwise.
+	public static boolean validPassword(String password) {  // boolean return function to validate password based on parameters,
+															// takes password as string as argument
+		String string = password;							// must be at least 8 characters, must contain uppercase, lowercase letters,
+															// at least 1 number and at least 1 special character
+															// returns true if password is valid, false otherwise.
 		final Pattern pattern = Pattern.compile("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$");
 		Matcher matcher = pattern.matcher(string);
 		
